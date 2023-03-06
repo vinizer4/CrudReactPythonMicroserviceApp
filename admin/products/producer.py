a7bdacf9d86f8f6c508ca31c8a6a074e7b06d1c0
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-03-06 16:40:26 -0300

    Add files via upload

---
+++ admin/products/producer.py
@@ -0,0 +1,13 @@
+# amqps://rvtlozhr:L_a5L2wFUpyFfW4L8e8JTc7-0n5cmeNH@jackal.rmq.cloudamqp.com/rvtlozhr
+import pika, json
+
+params = pika.URLParameters('amqps://rvtlozhr:L_a5L2wFUpyFfW4L8e8JTc7-0n5cmeNH@jackal.rmq.cloudamqp.com/rvtlozhr')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
